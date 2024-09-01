cf5be52eced1ee9294806754b55ec90c6e05efa3
andrewliu26 <aliu26@bu.edu>
2024-09-01 01:00:10 -0400

    responsive

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import Spline from '@splinetool/react-spline/next';
 
 export default function Home() {
     return (
-        <main>
-            <Spline
-                scene="https://prod.spline.design/i025hk0Vkw7V-Kgr/scene.splinecode"
-            />
+        <main style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+            <div style={{ backgroundColor: 'black', height: '100vh', width: '100%' }}>
+                <Spline
+                    scene="https://prod.spline.design/i025hk0Vkw7V-Kgr/scene.splinecode"
+                />
+            </div>
         </main>
     );
 }
