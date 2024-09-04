ae43e711a66b5dd47ba027b73ca3632aac22bc4a
andrewliu26 <aliu26@bu.edu>
2024-09-04 00:27:31 -0400

    glassmorphism

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import { InView } from "@/components/in-view";
 import { TextEffect } from "@/components/text-effect";
 import { Typewriter } from 'react-simple-typewriter';
 import { Poppins } from "next/font/google";
 import { IonIcon } from "@ionic/react";
 import { logoLinkedin, logoGithub, logoFigma, logoInstagram } from 'ionicons/icons';
 import { motion } from "framer-motion";
-import CursorEffect from "@/components/cursor-effect"; // Import the custom cursor
+import CursorEffect from "@/components/cursor-effect";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
 
@@ -15,12 +14,15 @@ export default function Page() {
     return (
         <main className="bg-black">
             <CursorEffect />
+
+            {/* Fade In Animation */}
             <motion.div
-                initial={{opacity: 0}}
-                animate={{opacity: 1}}
-                transition={{duration: 0.5, ease: "easeInOut", delay: 0.5}}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5, ease: "easeInOut", delay: 0.5 }}
                 className="h-screen w-screen overflow-auto"
             >
+                {/* Hero Section */}
                 <div className="h-screen flex flex-col text-white items-center justify-center bg-black z-20">
                     <TextEffect preset="fade"
                                 className={`py-6 text-center text-3xl font-bold ${poppins.className} z-20`}>
@@ -47,90 +49,80 @@ export default function Page() {
                     {/* Social Media Icons */}
                     <div className="flex space-x-4 my-10 z-20">
                         <a href="https://www.linkedin.com/in/andrewliu26/" target="_blank" rel="noopener noreferrer"
-                           className="relative group">
+                           className="relative group w-10 h-10 flex items-center justify-center">
                             <span
-                                className="absolute inset-0 bg-white opacity-0 rounded-md group-hover:opacity-30 transition-opacity duration-300"></span>
-                            <IonIcon icon={logoLinkedin} style={{fontSize: '24px', color: 'white'}}
+                                className="absolute inset-0 bg-white opacity-0 rounded-md group-hover:opacity-30 transition-opacity duration-300 w-10 h-10">
+                            </span>
+                            <IonIcon icon={logoLinkedin} style={{ fontSize: '24px', color: 'white' }}
                                      className="relative z-10"/>
                         </a>
                         <a href="https://github.com/andrewliu26" target="_blank" rel="noopener noreferrer"
-                           className="relative group">
+                           className="relative group w-10 h-10 flex items-center justify-center">
                             <span
-                                className="absolute inset-0 bg-white opacity-0 rounded-md group-hover:opacity-30 transition-opacity duration-300"></span>
-                            <IonIcon icon={logoGithub} style={{fontSize: '24px', color: 'white'}}
+                                className="absolute inset-0 bg-white opacity-0 rounded-md group-hover:opacity-30 transition-opacity duration-300 w-10 h-10">
+                            </span>
+                            <IonIcon icon={logoGithub} style={{ fontSize: '24px', color: 'white' }}
                                      className="relative z-10"/>
                         </a>
                         <a href="https://www.figma.com/@andrewliu26" target="_blank" rel="noopener noreferrer"
-                           className="relative group">
+                           className="relative group w-10 h-10 flex items-center justify-center">
                             <span
-                                className="absolute inset-0 bg-white opacity-0 rounded-md group-hover:opacity-30 transition-opacity duration-300"></span>
-                            <IonIcon icon={logoFigma} style={{fontSize: '24px', color: 'white'}}
+                                className="absolute inset-0 bg-white opacity-0 rounded-md group-hover:opacity-30 transition-opacity duration-300 w-10 h-10">
+                            </span>
+                            <IonIcon icon={logoFigma} style={{ fontSize: '24px', color: 'white' }}
                                      className="relative z-10"/>
                         </a>
                         <a href="https://www.instagram.com/andrewliu.u" target="_blank" rel="noopener noreferrer"
-                           className="relative group">
+                           className="relative group w-10 h-10 flex items-center justify-center">
                             <span
-                                className="absolute inset-0 bg-white opacity-0 rounded-md group-hover:opacity-30 transition-opacity duration-300"></span>
-                            <IonIcon icon={logoInstagram} style={{fontSize: '24px', color: 'white'}}
+                                className="absolute inset-0 bg-white opacity-0 rounded-md group-hover:opacity-30 transition-opacity duration-300 w-10 h-10">
+                            </span>
+                            <IonIcon icon={logoInstagram} style={{ fontSize: '24px', color: 'white' }}
                                      className="relative z-10"/>
                         </a>
                     </div>
-
                 </div>
 
-                {/* Experience */}
+                {/* Experience Section */}
                 <div className="h-screen flex flex-col items-center justify-center bg-black">
-                    <InView
-                        variants={{
-                            hidden: {opacity: 0, y: 50, filter: "blur(10px)"},
-                            visible: {opacity: 1, y: 0, filter: "blur(0px)"},
-                        }}
-                        viewOptions={{margin: "0px 0px -200px 0px"}}
-                        transition={{duration: 0.3, ease: "easeInOut"}}
-                    >
-                        <div className={`text-center text-white ${poppins.className}`}>
-                            <h1 className="font-bold text-2xl py-6">
-                                Experience
-                            </h1>
-                        </div>
-                    </InView>
+                    <div className="h-[90%] w-[90%] text-center text-white p-10 glass-div rounded-lg">
+                        <h1 className="font-bold text-2xl py-6">
+                            Experience
+                        </h1>
+                        <p className="text-sm">
+                            Details about your experience go here.
+                        </p>
+                    </div>
                 </div>
 
-                {/* Projects */}
+                {/* Projects Section */}
                 <div className="h-screen flex flex-col items-center justify-center bg-black">
-                    <InView
-                        variants={{
-                            hidden: {opacity: 0, y: 50, filter: "blur(10px)"},
-                            visible: {opacity: 1, y: 0, filter: "blur(0px)"},
-                        }}
-                        viewOptions={{margin: "0px 0px -200px 0px"}}
-                        transition={{duration: 0.3, ease: "easeInOut"}}
-                    >
-                        <div className={`text-center text-white ${poppins.className}`}>
-                            <h1 className="font-bold text-2xl py-6">
-                                Projects
-                            </h1>
-                        </div>
-                    </InView>
+                    <div className="h-[90%] w-[90%] text-center text-white p-10 glass-div rounded-lg">
+                        <h1 className="font-bold text-2xl py-6">
+                            Projects
+                        </h1>
+                        <p className="text-sm">
+                            Details about your projects go here.
+                        </p>
+                    </div>
                 </div>
 
-                {/* About Me */}
+                {/* About Me Section */}
+                {/*
                 <div className="h-screen flex flex-col items-center justify-center bg-gray-100">
-                    <InView
-                        variants={{
-                            hidden: {opacity: 0, y: 50, filter: "blur(10px)"},
-                            visible: {opacity: 1, y: 0, filter: "blur(0px)"},
-                        }}
-                        viewOptions={{margin: "0px 0px -200px 0px"}}
-                        transition={{duration: 0.3, ease: "easeInOut"}}
-                    >
-                        <div className={`text-center text-black ${poppins.className}`}>
-                            <h1 className="font-bold text-2xl py-6">
-                                Experience
-                            </h1>
-                        </div>
-                    </InView>
+                    <div className="text-center text-black p-10 glass-div rounded-lg">
+                        <h1 className="font-bold text-2xl py-6">
+                            About Me
+                        </h1>
+                        <p className="text-sm">
+                            Information about you goes here.
+                        </p>
+                    </div>
                 </div>
+                */}
+
+                {/* Footer */}
+                <div className="w-screen h-[20%] flex flex-col items-center justify-center bg-gray-100"/>
             </motion.div>
         </main>
     );
