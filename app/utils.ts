48b7c4d42b2fb651eb35247fb21d702a44b3a300
andrewliu26 <aliu26@bu.edu>
2024-09-17 13:25:49 -0400

    updated targetDate declaration

--- app/utils.ts
+++ app/utils.ts
@@ -2,7 +2,7 @@ export function formatDate(date: string) {
     if (!date.includes('T')) {
         date = `${date}T00:00:00`
     }
-    let targetDate = new Date(date)
+    const targetDate = new Date(date)
 
     return targetDate.toLocaleString('en-us', {
         month: 'short', // Abbreviated month (e.g., "Jul")
